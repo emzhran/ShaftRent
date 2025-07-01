27b1ef393e3576f3be7f9cf1a6f68d4d8b948a78
emzhran <zachran750@gmail.com>
2025-07-01 18:53:39 +0700

    membuat login event

---
+++ lib/presentation/auth/login/login_event.dart
@@ -0,0 +1,9 @@
+import 'package:shaft_rent/data/model/request/auth/login_request_model.dart';
+
+sealed class LoginEvent {}
+
+class LoginRequested extends LoginEvent {
+  final LoginRequestModel requestModel;
+
+  LoginRequested({required this.requestModel});
+}
