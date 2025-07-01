d1a24d7e5f045b50f5cba574f36a1fa9caf142ee
emzhran <zachran750@gmail.com>
2025-07-02 03:48:19 +0700

    menambahkan listview builder untuk menampilkan data mobil

--- lib/presentation/admin/car/widget/car_screen.dart
+++ lib/presentation/admin/car/widget/car_screen.dart
@@ -1,6 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shaft_rent/core/components/spaces.dart';
 import 'package:shaft_rent/core/constants/colors.dart';
+import 'package:shaft_rent/presentation/admin/car/getcar/getcar_bloc.dart';
+import 'package:shaft_rent/presentation/admin/car/getcar/getcar_state.dart';
 
 class CarScreen extends StatelessWidget {
   const CarScreen({super.key});
@@ -75,6 +79,117 @@ class CarScreen extends StatelessWidget {
               ), 
             ),
           ),
+          Expanded(
+            child: BlocBuilder<GetCarBloc, GetCarState>(
+              builder: (context, state) {
+                if (state is GetCarLoading) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (state is GetCarLoaded) {
+                  final cars = state.cars.data;
+                  if (cars.isEmpty) {
+                    return const Center(
+                      child: Text('Belum ada mobil tersedia. \nSilahkan tambahkan mobil baru.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: AppColors.black,
+                        fontSize: 16),
+                      ),
+                    );
+                  }
+                  return ListView.builder(
+                    padding: const EdgeInsets.only(bottom: 20),
+                    itemCount: cars.length,
+                    itemBuilder: (context, index) {
+                      final car = cars[index];
+                      return Card(
+                        margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadiusGeometry.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (car.gambarmobil != null && car.gambarmobil!.isNotEmpty)
+                              ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: CachedNetworkImage(
+                                  imageUrl: car.gambarmobil!,
+                                  height: 150,
+                                  width: double.infinity,
+                                  fit: BoxFit.cover,
+                                  placeholder: (context, url) => const Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                  errorWidget: (context, url, error) => const Icon(
+                                    Icons.image_not_supported,
+                                    size: 100,
+                                    color: AppColors.grey
+                                  ),
+                                ),
+                              )
+                              else
+                              Container(
+                                height: 150,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: AppColors.grey,
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.image_outlined,
+                                    size: 100,
+                                    color: AppColors.grey
+                                  ),
+                                ),
+                              ),
+                              const SpaceHeight(10),
+                              Text(
+                                car.namaMobil,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                '${car.merkMobil} - ${car.transmisi}',
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'Harga: Rp${car.hargaMobil.toInt()}/hari',
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                'Jumlah: ${car.jumlahMobil} unit'
+                              ),
+                              Text(
+                                'Kursi: ${car.jumlahKursi}'
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                  );
+                } else if (state is GetCarError) {
+                  return Center(
+                    child: Text('Error memuat data mobil: ${state.message}'),
+                  );
+                } else {
+                  return const Center(child: Text('Memuat data mobil...'));
+                }
+              }
+            )
+          )
         ],
       ),
     );
