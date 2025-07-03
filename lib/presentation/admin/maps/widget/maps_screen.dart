fd7c5af322e53e24c083a11362c8ca6db7082db8
emzhran <zachran750@gmail.com>
2025-07-03 16:16:50 +0700

    membuat maps screen dan menambahkan row untuk search data mobil

---
+++ lib/presentation/admin/maps/widget/maps_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:shaft_rent/core/components/spaces.dart';
+import 'package:shaft_rent/core/constants/colors.dart';
+
+class MapsScreen extends StatelessWidget {
+  const MapsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.card,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 24, left: 24, right: 24, bottom: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: AppColors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Row(
+                      children: [
+                        Icon(Icons.search, color: AppColors.black),
+                        SpaceWidth(8),
+                        Expanded(
+                          child: Text(
+                            'Cari Lokasi...',
+                            style: TextStyle(color: AppColors.grey, fontSize: 16),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
