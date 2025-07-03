748b6fb80297370ca928a46d8f333d48ea81fde0
emzhran <zachran750@gmail.com>
2025-07-03 21:32:37 +0700

    menambahkan textfield untuk input nama lokasi dan google maps untuk pilih lokasi

--- lib/presentation/admin/maps/widget/add_maps_screen.dart
+++ lib/presentation/admin/maps/widget/add_maps_screen.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:shaft_rent/core/components/custom_text_field.dart';
+import 'package:shaft_rent/core/components/spaces.dart';
+import 'package:shaft_rent/core/constants/colors.dart';
+import 'package:shaft_rent/data/model/request/admin/maps_request_model.dart';
+import 'package:shaft_rent/presentation/admin/maps/addmaps/addmaps_bloc.dart';
+import 'package:shaft_rent/presentation/admin/maps/addmaps/addmaps_event.dart';
+import 'package:shaft_rent/presentation/admin/maps/addmaps/addmaps_state.dart';
 
 class AddMapsScreen extends StatefulWidget {
   const AddMapsScreen({super.key});
@@ -13,7 +21,7 @@ class _AddMapsScreenState extends State<AddMapsScreen> {
   final namaLokasiController = TextEditingController();
   LatLng? selectedPosition;
 
-  final LatLng initialPosition = const LatLng(-7.7956, 110.3695);
+  final LatLng initialPosition = const LatLng(-7.810721359321624, 110.32186156987228);
 
   Future<void> requestLocationPermission() async {
     final status = await Permission.location.request();
@@ -38,6 +46,101 @@ class _AddMapsScreenState extends State<AddMapsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tambah Lokasi'),
+        centerTitle: true,
+        backgroundColor: AppColors.primary,
+        foregroundColor: AppColors.white,
+      ),
+      backgroundColor: AppColors.card,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: CustomTextField(
+              controller: namaLokasiController,
+              label: 'Nama Lokasi',
+              showLabel: false,
+              validator: 'Nama Lokasi tidak boleh kosong',
+              textColor: AppColors.black,
+              labelColor: AppColors.black,
+              borderColor: AppColors.black,
+              prefixIcon: Icon(Icons.map_outlined, color: AppColors.black),
+            ),
+          ),
+          Expanded(
+            child: GoogleMap(
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              initialCameraPosition: CameraPosition(target: initialPosition, zoom: 15),
+              markers: selectedPosition != null
+                      ? {
+                        Marker(
+                          markerId: const MarkerId('selected'),
+                          position: selectedPosition!,
+                          draggable: true,
+                          onDragEnd: (pos) => setState(() => selectedPosition = pos)
+                        )
+                      }
+                      : {},
+              onTap: (pos) {
+                setState(() {
+                  selectedPosition = pos;
+                });
+              },
+            ),
+          ),
+          const SpaceHeight(10),
+          BlocConsumer<AddMapsBloc, AddMapsState>(
+            listener: (context, state) {
+              if (state is AddMapsSuccess) {
+                Navigator.of(context).pop(true);
+              } else if (state is AddMapsError) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('Gagal menambahkan lokasi: ${state.message}'),
+                  backgroundColor: AppColors.red,
+                  ),
+                );
+              }
+            },
+            builder: (context, state) {
+              final isLoading = state is AddMapsLoading;
+              return Padding(
+                padding: const EdgeInsets.all(16),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primary,
+                    foregroundColor: AppColors.white,
+                    minimumSize: const Size.fromHeight(50),
+                  ),
+                  onPressed: isLoading
+                        ? null
+                        : () {
+                          if (namaLokasiController.text.isEmpty || selectedPosition == null) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Nama lokasi dan titik lokasi wajib diisi'),
+                                backgroundColor: AppColors.red)
+                            );
+                            return;
+                          }
+                          final request = MapsRequestModel(
+                            namaLokasi: namaLokasiController.text.trim(),
+                            latitude: selectedPosition!.latitude,
+                            longitude: selectedPosition!.longitude
+                          );
+                          context.read<AddMapsBloc>().add(AddMaps(requestModel: request)
+                          );
+                        }, 
+                  child: Text(isLoading ? 'Menyimpan...' : 'Simpan Lokasi')
+                  ),
+                );
+            }
+          ),
+          SpaceHeight(10)
+        ],
+      ),
+    );
   }
 }
