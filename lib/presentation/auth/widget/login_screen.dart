6db7ee2ffc980ec20e6273590f9a64310074029c
emzhran <zachran750@gmail.com>
2025-07-07 20:29:38 +0700

    menambahkan textfield untuk input email dan password

--- lib/presentation/auth/widget/login_screen.dart
+++ lib/presentation/auth/widget/login_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shaftrent/core/components/custom_text_field.dart';
+import 'package:shaftrent/core/components/spaces.dart';
+import 'package:shaftrent/core/constants/constants.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -31,6 +34,62 @@ class _LoginScreenState extends State<LoginScreen> {
   
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                const SpaceHeight(230),
+                Image.asset(
+                  'assets/images/shaft-logo.png',
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.contain,
+                ),
+                const SpaceHeight(30),
+                CustomTextField(
+                  controller: emailController,
+                  label: 'Email',
+                  showLabel: false,
+                  validator: 'Email tidak boleh kosong',
+                  keyboardType: TextInputType.emailAddress,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.email, color: AppColors.white),
+                  ),
+                ),
+                const SpaceHeight(25),
+                CustomTextField(
+                  controller: passwordController,
+                  label: 'Password',
+                  showLabel: false,
+                  obscureText: !isShowPassword,
+                  validator: 'Password tidak boleh kosong',
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.lock, color: AppColors.white),
+                  ),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        isShowPassword = !isShowPassword;
+                      });
+                    },
+                    icon: Icon(
+                      isShowPassword ? Icons.visibility : Icons.visibility_off,
+                      color: AppColors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
