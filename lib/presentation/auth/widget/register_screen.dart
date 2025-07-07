7cd7f17a29e6eca1dbddeed1681e0546c381a4d6
emzhran <zachran750@gmail.com>
2025-07-07 20:02:38 +0700

    membuat register screen dan menambahkan controller

---
+++ lib/presentation/auth/widget/register_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/widgets.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  late final TextEditingController namaController;
+  late final TextEditingController emailController;
+  late final TextEditingController passwordController;
+    bool isShowPassword = false;
+  late final GlobalKey<FormState> _key;
+
+ @override
+  void initState() {
+    namaController = TextEditingController();
+    emailController = TextEditingController();
+    passwordController = TextEditingController();
+    _key = GlobalKey<FormState>();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    namaController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
