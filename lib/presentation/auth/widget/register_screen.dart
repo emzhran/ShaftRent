e04fcd225fbc778398a4faaecbd446e8d7ae26cd
emzhran <zachran750@gmail.com>
2025-07-07 20:05:53 +0700

    menambahkan textfield untuk input nama

--- lib/presentation/auth/widget/register_screen.dart
+++ lib/presentation/auth/widget/register_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shaftrent/core/components/custom_text_field.dart';
+import 'package:shaftrent/core/components/spaces.dart';
+import 'package:shaftrent/core/constants/colors.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -33,6 +37,48 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _key,
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SpaceHeight(100),
+                Image.asset(
+                  'assets/images/shaft-logo.png',
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.contain,
+                ),
+                const SpaceHeight(40),
+                Text(
+                  'DAFTAR AKUN BARU',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                    color: AppColors.white
+                  ),
+                ),
+                const SpaceHeight(30),
+                CustomTextField(
+                  controller: namaController, 
+                  label: 'Nama',
+                  showLabel: false, 
+                  validator: 'Nama tidak boleh kosong',
+                  keyboardType: TextInputType.text,
+                  prefixIcon: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(Icons.person, color: AppColors.white)
+                  ),
+                ),
+              ],
+            ),
+          )
+        ),
+      ),
+    );
   }
 }
