237e376192d85d73d538e88aaeb4c3cafa2c8814
emzhran <zachran750@gmail.com>
2025-07-10 16:38:56 +0700

    menambahkan method get all customer, update status customer dan get customer detail

--- lib/data/repository/profile_repository.dart
+++ lib/data/repository/profile_repository.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 import 'package:dartz/dartz.dart';
+import 'package:shaftrent/data/model/request/admin/profile_customer_request_model.dart';
 import 'package:shaftrent/data/model/request/customer/profile_request_model.dart';
+import 'package:shaftrent/data/model/response/admin/profile_customer_response_model.dart';
 import 'package:shaftrent/data/model/response/customer/profile_response_model.dart';
 import 'package:shaftrent/service/service_http_client.dart';
 
@@ -40,4 +42,56 @@ class ProfileRepository {
       return Left('Terjadi kesalahan saat menyimpan profil: ${e.toString()}');
     }
   }
+
+  Future<Either<String, ProfileCustomerResponseModel>> getAllCustomers() async {
+    try {
+      final response = await _serviceHttpClient.get('admin/customers');
+      final jsonResponse = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        final model = ProfileCustomerResponseModel.fromJson(jsonResponse);
+        return Right(model);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Gagal memuat data customer.');
+      }
+    } catch (e) {
+      return Left('Terjadi kesalahan saat memuat data customer: ${e.toString()}');
+    }
+  }
+
+
+  Future<Either<String, ProfileCustomerResponseModel>> updateStatusAccount(
+      StatusAccountRequestModel request,    
+      ) async {
+        try {
+          final response = await _serviceHttpClient.put(
+            'admin/customers/${request.customerId}/status',request.toMap(),
+          );
+          final jsonResponse = json.decode(response.body);
+          if (response.statusCode == 200) {
+            final model = ProfileCustomerResponseModel.fromJson(jsonResponse);
+            return Right(model);
+          } else {
+            return Left(jsonResponse['message'] ?? 'Gagal memperbarui status akun');
+          }
+        } catch (e) {
+          return Left('Terjadi kesalahan: ${e.toString()}');
+        }
+      }
+
+    Future<Either<String, ProfileCustomer>> getCustomerDetail(int id) async {
+      try {
+        final response = await _serviceHttpClient.get('admin/customers/$id');
+        final jsonResponse = json.decode(response.body);
+
+        if (response.statusCode == 200) {
+          final profile = ProfileCustomer.fromJson(jsonResponse['data']);
+          return Right(profile);
+        } else {
+          return Left(jsonResponse['message'] ?? 'Gagal memuat data customer.');
+        }
+      } catch (e) {
+        return Left('Terjadi kesalahan: ${e.toString()}');
+      }
+    }
 }
