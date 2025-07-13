4559a1f56f9dd288d8224295ea0ba68249f6a747
emzhran <zachran750@gmail.com>
2025-07-13 16:41:25 +0700

    membuat history order repository dan response

---
+++ lib/data/model/response/customer/history_order_response.dart
@@ -0,0 +1,58 @@
+class HistoryOrder {
+  final int id;
+  final int userId;
+  final Car? car;
+  final DateTime tanggalMulai;
+  final DateTime tanggalSelesai;
+  final String metodePembayaran;
+  final String statusPemesanan;
+
+  HistoryOrder({
+    required this.id,
+    required this.userId,
+    this.car,
+    required this.tanggalMulai,
+    required this.tanggalSelesai,
+    required this.metodePembayaran,
+    required this.statusPemesanan,
+  });
+
+  factory HistoryOrder.fromJson(Map<String, dynamic> json) {
+    return HistoryOrder(
+      id: json['id'],
+      userId: json['user_id'],
+      car: json['car'] != null ? Car.fromJson(json['car']) : null,
+      tanggalMulai: DateTime.parse(json['tanggal_mulai']),
+      tanggalSelesai: DateTime.parse(json['tanggal_selesai']),
+      metodePembayaran: json['metode_pembayaran'],
+      statusPemesanan: json['status_pemesanan'],
+    );
+  }
+}
+
+class Car {
+  final int id;
+  final String merkMobil;
+  final String namaMobil;
+  final String? transmisi;
+  final String? gambarMobil;
+
+  Car({
+    required this.id,
+    required this.merkMobil,
+    required this.namaMobil,
+    required this.transmisi,
+    this.gambarMobil,
+  });
+
+  factory Car.fromJson(Map<String, dynamic> json) {
+    return Car(
+      id: json['id'],
+      merkMobil: json['merk_mobil'],
+      namaMobil: json['nama_mobil'],
+      transmisi: json['transmisi'],
+      gambarMobil: json['gambar_mobil'],
+    );
+  }
+}
+
---
+++ lib/data/repository/history_order_repository.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:dartz/dartz.dart';
+import 'package:shaftrent/data/model/response/customer/history_order_response.dart';
+import 'package:shaftrent/service/service_http_client.dart';
+
+class HistoryOrderRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  HistoryOrderRepository(this._serviceHttpClient);
+
+  Future<Either<String, List<HistoryOrder>>> getHistoryOrders() async {
+    try {
+      final response = await _serviceHttpClient.get('customer/orders');
+      final jsonResponse = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        final List<HistoryOrder> orders = List<HistoryOrder>.from(
+          (jsonResponse['data'] as List).map((e) => HistoryOrder.fromJson(e)),
+        );
+        return Right(orders);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Gagal memuat riwayat pesanan.');
+      }
+    } catch (e) {
+      return Left('Terjadi kesalahan saat memuat riwayat pesanan: ${e.toString()}');
+    }
+  }
+}
